90d9e9a9c0d31996ebf6225f3122d96c0056ef98
igor <idupalmieri@hotmail.com>
2025-05-05 20:05:09 -0300

    first commit

---
+++ src/main/java/dev/java/CadastroEmpresas/CadastroEmpresasApplication.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroEmpresas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEmpresasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEmpresasApplication.class, args);
+	}
+
+}
