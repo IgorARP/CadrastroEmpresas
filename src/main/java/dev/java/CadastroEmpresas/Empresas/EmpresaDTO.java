0b595ba7ca43f7ff5860e4d4189697abd34d952a
igor <idupalmieri@hotmail.com>
2025-05-07 21:01:50 -0300

    Chore: inclui a versão DTO pela entidade e mapeia manualmente no EmpresaMappper os caminhos da entidade e DTO

--- src/main/java/dev/java/CadastroEmpresas/Empresas/EmpresaDTO.java
+++ src/main/java/dev/java/CadastroEmpresas/Empresas/EmpresaDTO.java
@@ -1,4 +1,69 @@
 package dev.java.CadastroEmpresas.Empresas;
 
+import dev.java.CadastroEmpresas.Servicos.ServicoModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmpresaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int cnpj;
+    private ServicoModel servico;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public ServicoModel getServico() {
+        return servico;
+    }
+
+    public void setServico(ServicoModel servico) {
+        this.servico = servico;
+    }
 }
+
+
--- src/main/java/dev/java/CadastroEmpresas/Empresas/EmpresaMapper.java
+++ src/main/java/dev/java/CadastroEmpresas/Empresas/EmpresaMapper.java
@@ -1,4 +1,31 @@
 package dev.java.CadastroEmpresas.Empresas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmpresaMapper {
+
+    public EmpresaModel map(EmpresaDTO empresaDTO) {
+        EmpresaModel empresaModel = new EmpresaModel();
+        empresaModel.setId(empresaDTO.getId());
+        empresaModel.setNome(empresaDTO.getNome());
+        empresaModel.setEmail(empresaDTO.getEmail());
+        empresaModel.setCnpj(empresaDTO.getCnpj());
+        empresaModel.setImgUrl(empresaDTO.getImgUrl());
+        empresaModel.setServico(empresaDTO.getServico());
+
+        return empresaModel;
+    }
+
+    public EmpresaDTO map(EmpresaModel empresaModel){
+        EmpresaDTO empresaDTO = new EmpresaDTO();
+        empresaDTO.setId(empresaModel.getId());
+        empresaDTO.setCnpj(empresaModel.getCnpj());
+        empresaDTO.setNome(empresaModel.getNome());
+        empresaDTO.setEmail(empresaModel.getEmail());
+        empresaDTO.setCnpj(empresaModel.getCnpj());
+        empresaDTO.setImgUrl(empresaModel.getImgUrl());
+
+        return empresaDTO;
+    }
 }
