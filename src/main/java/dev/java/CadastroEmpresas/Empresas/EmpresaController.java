f824ca9b2c3f55e5c5aeb4a09e266393719e092d
igor <idupalmieri@hotmail.com>
2025-05-07 22:24:16 -0300

    refactor: refatora o controler para incluir o responceentity

--- src/main/java/dev/java/CadastroEmpresas/Empresas/EmpresaController.java
+++ src/main/java/dev/java/CadastroEmpresas/Empresas/EmpresaController.java
@@ -1,5 +1,7 @@
 package dev.java.CadastroEmpresas.Empresas;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,38 +17,63 @@ public class EmpresaController {
     }
 
     @GetMapping("/boasVindas")
-    public String boasVindas(){
+    public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
     }
 
     //Adicionar Empresa(CREATE)
     @PostMapping("/criar")
-    public EmpresaDTO criarEmpresa(@RequestBody EmpresaDTO empresa){
-        return empresaService.criarEmpresa(empresa);
+    public ResponseEntity<String> criarEmpresa(@RequestBody EmpresaDTO empresa) {
+        EmpresaDTO novaempresa = empresaService.criarEmpresa(empresa);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Empresa Criada C0m sucesso: " + novaempresa.getNome() + " (ID):" + novaempresa.getId());
     }
 
     //Mostrar todas as Empresas (READ)
-    @GetMapping("/todos")
-    public List<EmpresaDTO> listarEmpresa(){
-        return empresaService.listarEmpresa();
+    @GetMapping("/listar")
+    public ResponseEntity<List<EmpresaDTO>> listarEmpresa() {
+        List<EmpresaDTO> empresas = empresaService.listarEmpresa();
+        return ResponseEntity.ok(empresas);
     }
 
     //Mostrar empresa por ID(READ)
     @GetMapping("/listar/{id}")
-    public EmpresaDTO listarEmpresaPorID(@PathVariable Long id){
-        return empresaService.listarEmpresaPorID(id);
+    public ResponseEntity<?> listarEmpresaPorID(@PathVariable Long id) {
+
+        EmpresaDTO empresas = empresaService.listarEmpresaPorID(id);
+
+        if (empresas != null){
+            return ResponseEntity.ok(empresas);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A empresa com esse id: "+id+" não foi encontrada nos nossos registros");
+        }
     }
 
     //Alterar dados das Empresas (UPDATE)
     @PutMapping("/alterar/{id}")
-    public EmpresaDTO alterarEmpresaPorID(@PathVariable Long id, @RequestBody EmpresaDTO empresaAtualizada){
-        return empresaService.atualizarEmpresa(id,empresaAtualizada);
+    public ResponseEntity<?> alterarEmpresaPorID(@PathVariable Long id, @RequestBody EmpresaDTO empresaAtualizada) {
+        EmpresaDTO empresas= empresaService.atualizarEmpresa(id, empresaAtualizada);
+        if (empresas !=null){
+            return ResponseEntity.ok(empresas);
+
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A empresa com esse id: "+id+" não foi encontrada nos nossos registros");
+        }
     }
 
     //Deletar Empresa (DELETE)
     @DeleteMapping("/deletar/{id}")
-    public void deletarEmpresaPorID(@PathVariable Long id){
-        empresaService.deletarEmpresaPorID(id);
-    }
+    public ResponseEntity<String> deletarEmpresaPorID(@PathVariable Long id) {
+        if (empresaService.listarEmpresaPorID(id) != null) {
+            empresaService.deletarEmpresaPorID(id);
+            return ResponseEntity.ok("Empresa deletada com sucesso ");
 
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A Empresa com a id: " + id + " Nao encontrada");
+        }
+    }
 }
+
